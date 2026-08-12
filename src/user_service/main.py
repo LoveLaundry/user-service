8ee65777f8f6523df547e24a6eae1a6f09e606e6
DulshanSiriwardhana <dulshansiriwardhanaofficial@gmail.com>
2026-08-12 12:36:28 +0530

    UP

--- src/user_service/main.py
+++ src/user_service/main.py
@@ -1,16 +1,10 @@
 from typing import Union
 
-<<<<<<< HEAD
 from fastapi import Depends, FastAPI, HTTPException, UploadFile, File, Form
-=======
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import bcrypt
-
-from fastapi import Depends, FastAPI, HTTPException
->>>>>>> 2035f1a9ad435fe5237c981b4371a5b21f60ffec
-from fastapi.middleware.cors import CORSMiddleware
 import base64
-from pathlib import Path
 
 from .config import DB_TYPE, DatabaseType
 from .repository import UserRepository
